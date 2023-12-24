0a69fd7c51a9c6fad91cc7f0263a8bb378d00d63
Your Name <you@example.com>
2023-12-24 19:16:08 +0200

    aaaaaaa

--- arwa3.py
+++ arwa3.py
@@ -1,16 +1,4 @@
-# gen=int(input("1-boy, 2-girl: "))
-# if gen==1:
-#     w=float(input("boy weight: "))
-#     if 4<=w <=5.5
-#     print("in range")
-# else:
-#     print("not in range")  
-# elif gen==2:
-#     w=float(input("the weight"))
-# if 2.5<=w<=4.5
-#        print("range ")
-#else:
-#print("not range ")
+
 answer = 0
 a = int(input("first number : "))
 for i in range (int(input("second number : "))):                                             
